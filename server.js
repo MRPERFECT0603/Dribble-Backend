9e50043fe233e2e8366ad4da50b65acb42aabe59
VivekShauryaa <166864682+VivekShauryaa@users.noreply.github.com>
2024-04-13 15:44:56 +0530

    Update server.js for CROS origin allowance

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
 app.use(cors({
     // origin: "http://localhost:3000",
     // origin: "https://661a5831e487e377b1cac370--dribblevivek.netlify.app",
-    // origin: " https://dribblevivek.netlify.app",
+    origin: " https://dribblevivek.netlify.app",
     credentials: true,
 }));
 
